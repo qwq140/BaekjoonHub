6e64436f9c4ec54779cb57dbf86389b59dd4d570
Park Jaebin <74044232+qwq140@users.noreply.github.com>
2024-09-05 01:57:43 +0900

    [level 1] Title: 폰켓몬, Time: 9.49 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,6 +1,6 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 05일 01:57:15
+2024년 09월 05일 01:57:41
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,22 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        int max = nums.length / 2;
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for(int num : nums) {
-            map.put(num, map.getOrDefault(num, 0)+1);
-        }
-        
-        if(map.size() > max) {
-            answer = max;
-        } else {
-            answer = map.size();
-        }
-        
         return answer;
     }
 }
